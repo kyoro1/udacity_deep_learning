a983d28d6005350905d20d287eb3959e55ef1b4e
Kyoichi Iwasaki <kyoro1@iwasaki-no-MacBook-Pro.local>
2016-01-30 15:20:02 +0900

    Modified softmax script by multiplying/dividing by 10

--- L1_softmax.py
+++ L1_softmax.py
@@ -20,3 +20,12 @@ scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
 
 plt.plot(x, softmax(scores).T, linewidth=2)
 plt.show()
+
+
+# multiply by 10 / divide by 10
+scores = np.array([3.0, 1.0, 0.2'])
+## multiply by 10
+print(softmax(scores * 10))
+
+## divide by 10
+print(softmax(scores / 10))
