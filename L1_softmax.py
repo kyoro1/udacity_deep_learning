4f13c38153e1f2d0350e9037c1b5952916fb637c
Kyoichi Iwasaki <kyoro1@iwasaki-no-MacBook-Pro.local>
2016-01-30 15:05:35 +0900

    Added softmax functions

---
+++ L1_softmax.py
@@ -0,0 +1,22 @@
+""""softmax."""
+
+scores = [3.0, 1.0, 0.2]
+
+import numpy as np
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    x1 = np.exp(x)
+    c1 = sum(x1)
+    return x1/c1
+#    pass  # TODO: Compute and return softmax(x)
+
+print(softmax(scores))
+
+# Plot softmax curves
+import matplotlib.pyplot as plt
+x = np.arange(-2.0, 6.0, 0.1)
+scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
+
+plt.plot(x, softmax(scores).T, linewidth=2)
+plt.show()
